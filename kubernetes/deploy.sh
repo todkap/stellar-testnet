e19363702c305079a07a8585321ced4e2846567e
TODD E. KAPLINGER <todkap@us.ibm.com>
2019-05-14 09:40:17 -0400

    Debugging attempts to work through issue with horizon bootstrapping with postgres

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -2,23 +2,26 @@
 
 kubectl delete -f ./
 
+helm delete horizon --purge
+
+helm install --name horizon \
+  --set persistence.enabled=false,global.postgresql.postgresqlUsername=postgres,global.postgresql.postgresqlPassword=abcd,global.postgresql.postgresqlDatabase=stellar-horizon \
+    stable/postgresql
+
+kubectl apply -f stellar-horizon-deployment.yaml
+kubectl apply -f stellar-horizon-service.yaml
+
+
+
 kubectl apply -f postgres-sc-configmap.yaml	
 kubectl apply -f postgres-sc-storage.yaml
 kubectl apply -f postgres-sc-deployment.yaml	
 kubectl apply -f postgres-sc-service.yaml
 
-sleep 120
-
-kubectl apply -f postgres-horizon-configmap.yaml
-kubectl apply -f postgres-horizon-storage.yaml
-kubectl apply -f postgres-horizon-deployment.yaml	
-kubectl apply -f postgres-horizon-service.yaml	
-
-sleep 120
-
 
 kubectl apply -f stellar-core-deployment.yaml
 kubectl apply -f stellar-core-service.yaml
 
-kubectl apply -f stellar-horizon-deployment.yaml
-kubectl apply -f stellar-horizon-service.yaml
+
+
+
